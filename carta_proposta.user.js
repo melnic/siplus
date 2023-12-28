bbfe4b49cca0ead5931e7fd7d62854a3e13cb258
melnic <melnic@users.noreply.github.com>
2023-12-28 13:49:36 -0300

    Melhorias implementadas

--- carta_proposta.user.js
+++ carta_proposta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gerador de Carta Proposta
 // @namespace    http://tampermonkey.net/
-// @version      23.12.17
+// @version      23.12.23
 // @description  Obtem dados para carta proposta e lança no clipboard
 // @author       You
 // @match        http://webapps.sorocaba.sescsp.org.br/siplan/*
@@ -21,7 +21,7 @@
 // Corrige ano nas cartas a partir de 2024
 // Corrigido nome de arquivo com macro
 
-waitForKeyElements ("#btn-export", inserirBotao);
+waitForKeyElements("#btn-export", inserirBotao);
 
 var adress = window.location.href;
 var patt = /96\d{12}/i;
@@ -39,13 +39,13 @@ const template_link = "ms-word:nft|u|https://sescsp.sharepoint.com/sites/NcleoAr
 
 var open = XMLHttpRequest.prototype.open;
 //alert('função foi injetada');
-XMLHttpRequest.prototype.open = function(method, url, async) {
+XMLHttpRequest.prototype.open = function (method, url, async) {
     //alert(url);
     if (url.indexOf('api/atividade/96') !== -1) {
         //alert('Solicitação obtida:', method, url);
 
         // Adicionar observador de eventos para a resposta
-        this.addEventListener('load', function() {
+        this.addEventListener('load', function () {
             resposta = this.responseText;
         });
     }
@@ -53,11 +53,11 @@ XMLHttpRequest.prototype.open = function(method, url, async) {
 };
 
 //Função para obter dados de Ações
-var getJSON = function(url, callback) {
+var getJSON = function (url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.responseType = 'json';
-    xhr.onload = function() {
+    xhr.onload = function () {
         var status = xhr.status;
         if (status === 200) {
             callback(null, xhr.response);
@@ -69,7 +69,7 @@ var getJSON = function(url, callback) {
 };
 
 //Como utilizar a função getJSON: Funcional CTRL + I Busca dados da ação
-$(document).bind('keydown', 'ctrl+i', function(event){
+$(document).bind('keydown', 'ctrl+i', function (event) {
 
     event.preventDefault();
     event.stopPropagation();
@@ -78,75 +78,92 @@ $(document).bind('keydown', 'ctrl+i', function(event){
 
 });
 
-// Ajustar função para alimentar dados gerais da ação
-// talvez criar objeto carta, com propriedades: titulo, complemento, datas_texto, contratos: array, ...
-function obterDadosCP(acao){
-    const r = /(Com )(.*?)[.,]/i;
-    const s = /Com ((Cia.?)?.+?)[,.]/ig;
-
-    //var dados_cartas = [];
-    var titulo = acao.nome;
-    var datas = gerar_texto_datas(acao.datas);
-
-    //  Criar rotina para verificar todos os contrato, loop em Array, propriedades nome: parcelas: total:
-    var texto_parcelas = gerar_texto_contratos(acao)[0]; //obtem primeiro item de valores de contrato
-
-    var total = texto_parcelas.total;
-    var parcelas = texto_parcelas.parcelas;
-    var contratado = texto_parcelas.nome;
-
-    gerarDivCorrecoes(['titulo=' + titulo, 'contratado=' + contratado, 'datas=' + datas, 'total=' + total, 'parcelas=' + parcelas]);
-}
-
-// Ajustar função para lidar com listagem de objetos de contratos
-// Usar estrutura do botão <button type="button" class="btn" data-toggle="dropdown">
-
-function inserirBotao(){
+function inserirBotao() {
+  
     var d = $('#btn-export').parent();
     d.append('<button class="btn" id="btn-carta" role="button" data-toggle="modal">Carta Proposta</button>');
     $("#btn-carta").click(function(){
         //Alterar função para alimentar variáveis gerais da ação
         obterDadosCP(JSON.parse(resposta));
     });
+
 }
 
+// Ajustar função para alimentar dados gerais da ação
+// talvez criar objeto carta, com propriedades: titulo, complemento, datas_texto, contratos: array, ...
+function obterDadosCP(acao) {
+    const r = /(Com )(.*?)[.,]/i;
+    const s = /Com ((Cia.?)?.+?)[,.]/ig;
+
+    //var dados_cartas = [];
+    var titulo = acao.nome;
+    var datas = gerar_texto_datas(acao.datas);
+    var total = "";
+    var parcelas = "";
+    var contratado = "";
+
+    //Gerar listagem com dados de contratos
+    var texto_parcelas = gerarTextosDosContratos(acao);
+
+    // Menu com Vários Contratos
+    if (texto_parcelas.length >= 1) {
+        //if (texto_parcelas.length){
+        total = texto_parcelas[0].total;
+        parcelas = texto_parcelas[0].parcelas;
+        contratado = texto_parcelas[0].contratado;
+    } else {
+        total = texto_parcelas[0].total;
+        parcelas = texto_parcelas[0].parcelas;
+        contratado = texto_parcelas[0].contratado;
+    }
+
+    //Cria Div com dados de um contrato
+    criarDivCopiaCola(['titulo=' + titulo, 'contratado=' + contratado, 'datas=' + datas, 'total=' + total, 'parcelas=' + parcelas]);
+}
+
+// Ajustar função para lidar com listagem de objetos de contratos
+// Usar estrutura do botão <button type="button" class="btn" data-toggle="dropdown">
+
+
+
 function toReais(numero) {
-    return numero.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
-  }
+    return numero.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+}
 
-// Retorna listagem de objetos
-function gerar_texto_contratos(acao){
-
-    var contratos = acao.servicos.filter(function(item){
-        return item.itemDescricao.toLowerCase().includes("contrato") || item.itemDescricao.toLowerCase().includes("licença"); 
-    });
+// Retorna listagem de objetos com dados de contratos
+function gerarTextosDosContratos(acao) {
 
     var texto = '';
-
     var retorno = [];
+
+    //Filtra ações com item contrato
+    var contratos = acao.servicos.filter(function (item) {
+        return item.itemDescricao.toLowerCase().includes("contrato") || item.itemDescricao.toLowerCase().includes("licença");
+    });
 
     contratos.forEach(contrato => {
 
         let n = 1;
         let texto_parcelas = '';
-        let contratado = contrato.descricao == null ? 'Nulo' : contrato.descricao;
+        let contratado = contrato.descricao ? contrato.descricao : 'Preencher';
 
-        if (contrato.parcelas != null){
-        contrato.parcelas.forEach(parcela => {
-            texto_parcelas += 'parcela ' + n + ', no valor de ' + toReais(parcela.valor) + ', a ser paga em ';
-            texto_parcelas += parcela.dataPrevista + '; ';
-            n++;
-        })
-        }else{
+        if (contrato.parcelas != null) {
+            contrato.parcelas.forEach(parcela => {
+                texto_parcelas += 'parcela ' + n + ', no valor de ' + toReais(parcela.valor) + ', a ser paga em ';
+                texto_parcelas += parcela.dataPrevista + '; ';
+                n++;
+            })
+        } else {
             texto_parcelas = "";
         }
-        retorno.push({'contratado':contrato.descricao,'total':toReais(contrato.custo), 'parcelas': texto_parcelas});
+        //retorno.push({'contratado':contrato.descricao,'total':toReais(contrato.custo), 'parcelas': texto_parcelas});
+        retorno.push({ 'contratado': contratado, 'total': toReais(contrato.custo), 'parcelas': texto_parcelas });
     })
 
     return retorno
 }
 
-function gerar_texto_datas(datas){
+function gerar_texto_datas(datas) {
     var texto = '';
     var ano = '';
 
@@ -159,7 +176,7 @@ function gerar_texto_datas(datas){
     var z = {}; //guarda dados sobre datas para gerar texto
 
     datas.forEach(data => {
-        
+
         //let ano = r_ano.exec(data.dataAgenda.dataInicio)[2];
         let mes = r_dia_mes.exec(data.dataAgenda.dataInicio)[2];
         let dia = r_dia_mes.exec(data.dataAgenda.dataInicio)[1];
@@ -168,11 +185,11 @@ function gerar_texto_datas(datas){
         let hora_fim = r_hora.exec(data.dataAgenda.dataFim);
         let horario = 'das ' + hora_inicio + ' às ' + hora_fim;
 
-        if (z[horario] == null){
+        if (z[horario] == null) {
             z[horario] = {};
         }
 
-        if (z[horario][mes] == null){
+        if (z[horario][mes] == null) {
             z[horario][mes] = [];
         }
 
@@ -180,10 +197,10 @@ function gerar_texto_datas(datas){
 
     })
 
-    for (let horario in z){
-        for (let mes in z[horario]){
+    for (let horario in z) {
+        for (let mes in z[horario]) {
             //texto += z[horario][mes].join(', ') + '/' + mes + '/' + ano + ', ' ;
-            texto += z[horario][mes].join(', ') + '/' + mes + ', ' ;
+            texto += z[horario][mes].join(', ') + '/' + mes + ', ';
         }
         texto += horario + '; ';
     }
@@ -199,37 +216,34 @@ function gerar_texto_datas(datas){
 }
 
 //Insere DIV com dados obtidos para criar Carta Proposta
-function gerarDivCorrecoes (dados_cp) {
-    'use strict';    
+function criarDivCopiaCola(dados_cp) {
+    'use strict';
 
+    //Gerar String para o ClipBoard
     let textos_cp = dados_cp.join('|');
 
     // Inserir Dados da ação em input
     var n = document.getElementById("link-anexos");
 
-    //cria parágrafo para armazenar dados da carta
-
     //Rotina para não inserir mais um div com infos
-    if (document.getElementById("dados_carta") == null){
+    if (document.getElementById("dados_carta") == null) {
         var ativ = document.createElement("p");
         ativ.innerHTML = '<input type="text" value="' + textos_cp + '" id="dados_carta" style="border:0px;font-size:10px;margin-left:-.5em;">';
         var myInput = document.getElementById("dados_carta");
         //n.insertBefore(ativ, n.firstChild);
         n.appendChild(ativ);
-    }else{
+    } else {
         ativ = document.getElementById('dados_carta');
         ativ.value = textos_cp;
     }
 
     //Função de copiar para ClipBoard
-    ativ.addEventListener('click', function() {
+    ativ.addEventListener('click', function () {
         //Copy text from input field to clipboard
         var copyText = document.getElementById("dados_carta");
         copyText.select();
         document.execCommand("copy");
     }, false);
-
-
     ativ.click();
     document.activeElement.blur();
     window.open(template_link);
